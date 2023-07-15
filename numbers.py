e5056d30b08ac973d5ee83ec6b52709975c58541
Massimiliano Sartorio <102861506+Defqon01@users.noreply.github.com>
2023-07-15 21:38:46 +0200

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,9 @@
+a = input ("insert first number: ")
+b = input ("insert second number: ")
+
+if a == b:
+    print("Numbers are equals")
+elif a > b:
+    print("greater number is" + str(a))
+else:
+    print("greater number is" + str(b))
